ce930f74401ac63e831344ad3150b2bcf19b2869
Gadi Maizler <gadi.maizler@bitmain.com>
2018-10-29 15:14:07 +0200

    debugging

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-// import * as ConfigManager from './configManager';
-// export default ConfigManager;
+import {Notifications, NotificationsString} from './notifications';
+import {orderTypes, orderTypesStr} from './orderTypes';
+import {exchangeIds} from './exchangeIds';
+
+module.exports.Notifications = Notifications;
+module.exports.NotificationsString = NotificationsString;
+module.exports.orderTypes = orderTypes;
+module.exports.orderTypesStr = orderTypesStr;
+module.exports.exchangeIds = exchangeIds;
+
+
